222a25afbc92357e7db3963296dce1331faaed32
vasya75-oss <vasya75@gmail.com>
2026-04-27 12:13:03 +0300

    Delete NutriAct_v4.0 directory

--- NutriAct_v4.0/utils.js
+++
@@ -1,9 +0,0 @@
-function formatDate(date) {
-    return new Date(date).toLocaleDateString('ru-RU');
-}
-
-function showNotification(title, body) {
-    if ('Notification' in window && Notification.permission === 'granted') {
-        new Notification(title, { body });
-    }
-}
